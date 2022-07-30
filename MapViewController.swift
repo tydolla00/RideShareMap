a12af59e6d945b95f7fdd2cc54cfd68d2f84cc48
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2022-07-29 21:21:31 -0400

    First Commit

---
+++ MapViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MapViewController.swift
+//  RideSharer
+//
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+
+            // For use in foreground
+            self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+            mapView.delegate = self
+            mapView.mapType = .standard
+            mapView.isZoomEnabled = true
+            mapView.isScrollEnabled = true
+
+            if let coor = mapView.userLocation.location?.coordinate{
+                mapView.setCenter(coor, animated: true)
+            }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+
+        mapView.mapType = MKMapType.standard
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "You"
+        annotation.subtitle = "current location"
+        mapView.addAnnotation(annotation)
+
+        //centerMap(locValue)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorization()
+    }
+    
+    func authorization(){
+        switch locationManager.authorizationStatus{
+        case .denied, .restricted:
+            let alert = UIAlertController(title: "Denied", message: "You have denied location permissions", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        default:
+            print("Hi world")
+        }
+    }
+}
